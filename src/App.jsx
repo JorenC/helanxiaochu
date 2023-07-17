69f3da070d5aec1b44243067802a039852402bc3
Joren <7293081+JorenC@users.noreply.github.com>
2023-07-17 10:55:58 +0200

    Chinese JSON!

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
 >> cook time: 20 minutes
 >> servings: 4
 
-Wash the @green beans{500%gr} and pat them dry with a kitchen towel. Heat @vegetable oil{¼ cup} in a #wok over medium high heat. Fry the beans in one layer (requires two batches). They're done once they appear wrinkled and slightly scorched, after about 5 minutes. Use a #strainer to remove the beans and set aside.
+Wash the @green beans{500%gr // top & bottom removed, halved} and pat them dry with a kitchen towel. Heat @vegetable oil{¼ cup // or sunflower oil} in a #wok over medium high heat. Fry the beans in one layer (requires two batches). They're done once they appear wrinkled and slightly scorched, after about 5 minutes. Use a #strainer to remove the beans and set aside.
 
-Turn off the heat and scoop the oil out of the pan, except for 1 tbsp. Turn the heat to low, and add the @sichuan peppercorns{2 tsp}, @ginger{1 tsp}, @garlic{3 cloves} and @dried red chilies{3}. Stir-fry for about 1 minute, until fragrant.
+Turn off the heat and scoop the oil out of the pan, except for 1 tbsp. Turn the heat to low, and add the @sichuan peppercorns{2 tsp}, @ginger{1 tsp // minced}, @garlic{3 cloves // minced} and @dried red chilies{3 // deseeded and sliced}. Stir-fry for about 1 minute, until fragrant.
 
-Next, add in the @ground pork or chicken{125 gr} and turn up the heat to high. Stir-fry quickly to break up the pork until the meat is browned slightly.
+Next, add in the @ground pork or chicken{125 gr // or vegetarian alternative} and turn up the heat to high. Stir-fry quickly to break up the pork until the meat is browned slightly.
 
 Add in the fried green beans, @Shiao Hsing wine{1 tbsp}, @light soy sauce{1 tbsp}, @dark soy sauce{¼ tsp}, and @sugar{¼ tsp}. Toss everything well, and add @salt{to taste}. Serve hot with steamed white rice and other dishes.`; // <- Your CookLang recipe
   const recipe = new Recipe(recipeString);
@@ -24,12 +24,23 @@ Add in the fried green beans, @Shiao Hsing wine{1 tbsp}, @light soy sauce{1 tbsp
         <div key={index}>
           <Ingredient
             name={ingredient.name}
-            chineseName="鸡蛋"
-            notes="Some notes about the ingredient"
+            notes={
+              ingredient.amount.includes("//") ||
+              ingredient.units.includes("//")
+                ? ingredient.amount.split("//")[1] ||
+                  ingredient.units.split("//")[1]
+                : ""
+            }
             amount={
-              isNaN(ingredient.quantity)
-                ? ingredient.amount
-                : ingredient.quantity + " " + ingredient.units
+              isNaN(ingredient.quantity) || ingredient.quantity === 0
+                ? ingredient.amount.includes("//")
+                  ? ingredient.amount.split("//")[0]
+                  : ingredient.amount
+                : ingredient.quantity +
+                  " " +
+                  (ingredient.units.includes("//")
+                    ? ingredient.units.split("//")[0]
+                    : ingredient.units)
             }
             index={index} // Pass the index as a prop
           />{" "}
--- src/components/ingredient/Ingredient.jsx
+++ src/components/ingredient/Ingredient.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "./Ingredient.css";
+import jsonData from "./chinesetranslations.json";
 
 function Ingredient(props) {
-	const { name, chineseName, notes, amount, index } = props;
+	const { name, notes, amount, index } = props;
 	const wrapperClassName = index % 2 === 0 ? "wrapper" : "wrapperred";
+	console.log(jsonData);
+	console.log(name);
+	const chineseName = jsonData[name] || ""; // Check if the name exists in the JSON
+
 	return (
 		<div className={wrapperClassName}>
 			<div className="left">
 				<div className="chineseWrapper">
 					<div className="name">{name}</div>
-					<div className="chineseName">{chineseName}</div>
+					{chineseName && (
+						<div className="chineseName">{chineseName}</div>
+					)}
 				</div>
 				<div className="notes">{notes}</div>
 			</div>
