77b65f3f4fca4e04f3697a1e4d373897544592a5
Joren <7293081+JorenC@users.noreply.github.com>
2023-07-27 00:26:39 +0800

    logo

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,14 +1,16 @@
 import recipesData from "../../assets/recipes/recipes.js"; // Import the recipesData object
 import { Link } from "react-router-dom";
 import RecipeCard from "../../components/recipecard/RecipeCard.jsx";
+import styles from "./MainPage.css";
 
 export default function MainPage() {
   return (
     <>
       <div id="mainpage">
-      <img src="../logo_large.svg"
-        <h1>荷兰小厨</h1>
-        HELAN XIAOCHU
+        <div className="mainPageHeader">
+          <img src="../logo_large.svg" height="50px" />
+          <h1>荷兰小厨</h1>
+        </div>
         {Object.keys(recipesData).map((recipeName) => (
           <RecipeCard key={recipeName} name={recipeName} />
         ))}
